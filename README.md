ae4856e97cccb0cb4c4c1beda0fd74a9f3320690
12211062mirariska <mirariska100@gmail.com>
2023-04-15 21:18:22 +0700

    ini pesan filenya

---
+++ README.md
@@ -0,0 +1,4 @@
+nim dan nama kelompok
+12211062_Mira Risak(ketua)
+12211168_Mujahiddah(anggota)
+12211261_Samiun(anggota)
---
+++ android/app/src/main/kotlin/id/ac/bsi/u4d_12211062/presensipegawai/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.ac.bsi.u4d_12211062.presensipegawai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
